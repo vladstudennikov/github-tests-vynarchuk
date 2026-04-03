e4b0c0e31370a64984727dbf70b2d895806d68ea
vladstudennikov <studennikovvlad@gmail.com>
2026-04-03 17:00:42 +0300

    fix tests

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,5 +1,9 @@
 import pytest
 from service import TaskService
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 def test_add_task_success():
     service = TaskService()
