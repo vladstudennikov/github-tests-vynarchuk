09269b98a5c2a18f3d5c8ed92f2c34f90afc5aa8
vladstudennikov <studennikovvlad@gmail.com>
2026-04-04 00:04:16 +0300

    Add error not to pass tests

--- validator.py
+++ validator.py
@@ -2,5 +2,5 @@ def validate_title(title: str):
     if title is None or title.strip() == "":
         raise ValueError("Title cannot be empty")
 
-    if len(title) < 3:
-        raise ValueError("Title must be at least 3 characters long")
+    # if len(title) < 3:
+        # raise ValueError("Title must be at least 3 characters long")
